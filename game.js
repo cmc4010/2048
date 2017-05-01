ecdc9af15769943c95b69c51149f5d74456007e7
Chao-Ming Chang <cmc4010@gmail.com>
2017-05-01 21:12:19 +0800

    Added gameover screen and conditions

--- game.js
+++ game.js
@@ -7,6 +7,8 @@
 // most efficient way is to adjust text node value and class
 
 var tilesDOM = document.getElementsByClassName("tiles")[0];
+var newGameButton = document.getElementById("b-newgame");
+var gameOverScreen = document.getElementById("gameover");
 
 var app = {
 	"initialize": function(){
@@ -23,7 +25,6 @@ var app = {
 		// this.moveLeft();
 
 		// Create nodes at 2 random spots
-
 		app.genNode();
 		app.genNode();
 
@@ -53,6 +54,17 @@ var app = {
 					break;
 			}
 		}
+		newGameButton.addEventListener('click',app.resetGame);
+	},
+	"resetGame": function(){
+		// clear game
+		while(tilesDOM.firstChild){
+			tilesDOM.removeChild(tilesDOM.firstChild);
+		}
+		gameOverScreen.style.display = "none";
+		// generate 2 random
+		app.genNode();
+		app.genNode();
 	},
 	"tileCount": function(){
 
@@ -94,7 +106,16 @@ var app = {
 		// create Node
 		app.newNode( 2 ** Math.floor((Math.random()*2)+1), row, col );
 		myCount.incCount();
-		console.log(myCount.getCount());
+		// check end condition
+		if(myCount.getCount() === 16){
+			var actionables = app.dummyLeft() + app.dummyRight() + app.dummyUp() + app.dummyDown();
+			if(!actionables){
+				setTimeout(function(){
+					// Gameover operations
+					gameOverScreen.style.display = "block";
+				}, 1000)
+			}
+		}
 	},
 	"newNode": function(num, x, y){
 		var myTile = document.createElement("div");
@@ -205,6 +226,71 @@ var app = {
 		}
 		return actionFlag;
 	},
+	"dummyLeft": function(){
+		// for checking purposes
+		// does not change game state
+		var i;
+		var j;
+		var actionFlag = 0;
+		for(i = 0; i < 4; i++){ // i = row
+			for(j = 0; j < 3; j++){ // j = column
+
+				// node on current spot
+				var curNode = document.getElementsByClassName("position-" + i + "-" + j)[0];
+				// search right
+				var k;
+				adjNode = undefined;
+				for(k = j+1; k < 4; k++){
+					adjNode = document.getElementsByClassName("position-" + i + "-" + k)[0];
+					if(adjNode){
+						break;
+					}
+				}
+				// attempt merge
+				if(curNode){
+					if(adjNode && curNode.childNodes[0].nodeValue === adjNode.childNodes[0].nodeValue){
+						// merge
+						actionFlag = 1;
+					}
+					else {
+						// do nothing
+					}
+				}
+				else if(adjNode) {
+					// empty grid with nodes to the right
+					// check for merge
+					// search further right
+					var t;
+					adjNode2 = undefined;
+					for (t = k+1; t < 4; t++){
+						var adjNode2 = document.getElementsByClassName("position-" + i + "-" + t)[0];
+						if(adjNode2){
+							break;
+						}
+					}
+					// attempt merge
+					if(adjNode2){
+						// perform merge
+						if(adjNode2.childNodes[0].nodeValue === adjNode.childNodes[0].nodeValue){
+							// merge
+						}
+						else {
+							// shift only
+						}
+					}
+					else {
+						// shift only
+					}
+					actionFlag = 1;
+				}
+				else {
+					// empty row
+					// do nothing
+				}
+			}
+		}
+		return actionFlag;
+	},
 	"moveRight": function(){
 		var i;
 		var j;
@@ -291,6 +377,78 @@ var app = {
 						// NODE FINAL PLACEMENT
 						adjNode.classList.remove(adjNode.classList[2]);
 						adjNode.classList.add("position-" + i + "-" + j);
+					}
+					actionFlag = 1;
+				}
+				else {
+					// empty row
+					// do nothing
+				}
+			}
+		}
+		return actionFlag;
+	},
+	"dummyRight": function(){
+		var i;
+		var j;
+		var actionFlag = 0;
+		for(i = 0; i < 4; i++){ // i = row
+			for(j = 3; j > 0; j--){ // j = column
+
+				// node on current spot
+				var curNode = document.getElementsByClassName("position-" + i + "-" + j)[0];
+				// console.log(i + "," + j + "== curNode: " + curNode);
+				// search right
+				var k;
+				adjNode = undefined;
+				for(k = j-1; k >= 0; k--){
+					adjNode = document.getElementsByClassName("position-" + i + "-" + k)[0];
+					if(adjNode){
+						break;
+					}
+				}
+				// console.log(i + "," + j + "== adjNode: " + adjNode);
+				
+				// attempt merge
+				if(curNode){
+					if(adjNode && curNode.childNodes[0].nodeValue === adjNode.childNodes[0].nodeValue){
+						// merge
+						actionFlag = 1;
+					}
+					else {
+						// do nothing
+					}
+				}
+				else if(adjNode) {
+					// empty with nodes to the right
+					// check for merge
+					// search further right
+					var t;
+					adjNode2 = undefined;
+					for (t = k-1; t >= 0; t--){
+						var adjNode2 = document.getElementsByClassName("position-" + i + "-" + t)[0];
+						if(adjNode2){
+							break;
+						}
+					}
+
+					// console.log(i + "," + j + "== adjNode2: " + adjNode2);
+
+					// attempt merge
+					if(adjNode2){
+						// perform merge
+						if(adjNode2.childNodes[0].nodeValue === adjNode.childNodes[0].nodeValue){
+							// update adjacent node
+							// merge
+						}
+						else {
+							// shift only
+						}
+					}
+					else {
+						// regular placement of adjNode
+						// shift only
+						// NODE FINAL PLACEMENT
 					}
 					actionFlag = 1;
 				}
@@ -399,6 +557,77 @@ var app = {
 		}
 		return actionFlag;
 	},
+	"dummyUp": function(){
+		var i;
+		var j;
+		var actionFlag = 0;
+		for(i = 0; i < 4; i++){ // i = column
+			for(j = 0; j < 3; j++){ // j = row
+
+				// node on current spot
+				var curNode = document.getElementsByClassName("position-" + j + "-" + i)[0];
+				// console.log(j + "," + i + "== curNode: " + curNode);
+				// search right
+				var k;
+				adjNode = undefined;
+				for(k = j+1; k < 4; k++){
+					adjNode = document.getElementsByClassName("position-" + k + "-" + i)[0];
+					if(adjNode){
+						break;
+					}
+				}
+				// console.log(j + "," + i + "== adjNode: " + adjNode);
+				
+				// attempt merge
+				if(curNode){
+					if(adjNode && curNode.childNodes[0].nodeValue === adjNode.childNodes[0].nodeValue){
+						// update adjacent node
+					}
+					else {
+						// do nothing
+					}
+				}
+				else if(adjNode) {
+					// empty with nodes to the right
+					// check for merge
+					// search further right
+					var t;
+					adjNode2 = undefined;
+					for (t = k+1; t < 4; t++){
+						var adjNode2 = document.getElementsByClassName("position-" + t + "-" + i)[0];
+						if(adjNode2){
+							break;
+						}
+					}
+
+					// console.log(j + "," + i + "== adjNode2: " + adjNode2);
+
+					// attempt merge
+					if(adjNode2){
+						// perform merge
+						if(adjNode2.childNodes[0].nodeValue === adjNode.childNodes[0].nodeValue){
+							// update adjacent node
+							// value * 2
+						}
+						else {
+							// shift only
+						}
+					}
+					else {
+						// regular placement of adjNode
+						// shift only
+						// NODE FINAL PLACEMENT
+					}
+					actionFlag = 1;
+				}
+				else {
+					// empty row
+					// do nothing
+				}
+			}
+		}
+		return actionFlag;
+	},
 	"moveDown":function(){
 		var i;
 		var j;
@@ -485,6 +714,78 @@ var app = {
 						// NODE FINAL PLACEMENT
 						adjNode.classList.remove(adjNode.classList[2]);
 						adjNode.classList.add("position-" + j + "-" + i);
+					}
+					actionFlag = 1;
+				}
+				else {
+					// empty row
+					// do nothing
+				}
+			}
+		}
+		return actionFlag;
+	},
+	"dummyDown": function(){
+		var i;
+		var j;
+		var actionFlag = 0;
+		for(i = 0; i < 4; i++){ // i = col
+			for(j = 3; j > 0; j--){ // j = row
+
+				// node on current spot
+				var curNode = document.getElementsByClassName("position-" + j + "-" + i)[0];
+				// console.log(j + "," + i + "== curNode: " + curNode);
+				// search right
+				var k;
+				adjNode = undefined;
+				for(k = j-1; k >= 0; k--){
+					adjNode = document.getElementsByClassName("position-" + k + "-" + i)[0];
+					if(adjNode){
+						break;
+					}
+				}
+				// console.log(j + "," + i + "== adjNode: " + adjNode);
+				
+				// attempt merge
+				if(curNode){
+					if(adjNode && curNode.childNodes[0].nodeValue === adjNode.childNodes[0].nodeValue){
+						// update adjacent node
+						actionFlag = 1;
+					}
+					else {
+						// do nothing
+					}
+				}
+				else if(adjNode) {
+					// empty with nodes to the right
+					// check for merge
+					// search further right
+					var t;
+					adjNode2 = undefined;
+					for (t = k-1; t >= 0; t--){
+						var adjNode2 = document.getElementsByClassName("position-" + t + "-" + i)[0];
+						if(adjNode2){
+							break;
+						}
+					}
+
+					// console.log(j + "," + i + "== adjNode2: " + adjNode2);
+
+					// attempt merge
+					if(adjNode2){
+						// perform merge
+						if(adjNode2.childNodes[0].nodeValue === adjNode.childNodes[0].nodeValue){
+							// update adjacent node
+							// value * 2
+						}
+						else {
+							// shift only
+						}
+					}
+					else {
+						// regular placement of adjNode
+						// shift only
+						// NODE FINAL PLACEMENT
 					}
 					actionFlag = 1;
 				}
